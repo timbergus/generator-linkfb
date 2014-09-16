370bc8267e45f7fce8b3c5cd0f3c9616eee5434f
timbergus <timbergus@gmail.com>
2014-09-16 11:57:48 +0200

    Arrgggh!

--- test/test-creation.js
+++ test/test-creation.js
@@ -23,11 +23,11 @@ describe('linkfb generator', function () {
             '../../.travis.yml',
             '../../package.json',
             '../../README.md',
-            '../../app/templates/_README.js',
-            '../../app/templates/_bower.js',
+            '../../app/templates/_README.md',
+            '../../app/templates/_bower.json',
             '../../app/templates/_gruntfile.js',
             '../../app/templates/_linkfb.js',
-            '../../app/templates/_package.js'
+            '../../app/templates/_package.json'
         ];
 
         helpers.mockPrompt(this.app, {
